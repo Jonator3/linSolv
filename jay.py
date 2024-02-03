f8786188e8bff167cd85565c6309b5959dcab041
Jonator3 <joey.pehlke@gmail.com>
2024-02-03 19:55:50 +0100

    add jay's solve_dio and kgV

--- jay.py
+++ jay.py
@@ -1,5 +1,5 @@
 import math
-from typing import List
+from typing import List, Tuple
 
 
 def mat_(mat, n, k):
@@ -20,15 +20,38 @@ def det(mat: List[List[float]]) -> float:
     return sum([((-1)**k) * mat[0][k] * det(mat_(mat, 0, k)) for k in range(n)])
 
 
-def ggT(a: int, b: int) -> int:
+def ggT_mxy(a: int,b: int,) -> Tuple[int, int, int]:
     if a < b:
-        return ggT(b, a)
-    q = math.floor(a/b)
-    c = (-q)*b+a
-    if c == 0:
-        return b
-    else:
-        return ggT(b, c)
+        return ggT_mxy(b, a)
+
+    m = [a, b]
+    x = [1, 0]
+    y = [0, 1]
+    while m[-1] != 0:
+        q = math.floor(m[-2]/m[-1])
+        m.append((-q)*m[-1]+m[-2])
+        x.append(x[-2]-(q*x[-1]))
+        y.append(x[-2]-(q*y[-1]))
+        if len(m) > 32:
+            m = m[-5:]
+            x = x[-5:]
+            y = y[-5:]
+    return m[-2], x[-2], y[-2]
+
+
+def ggT(a: int, b: int) -> int:
+    return ggT_mxy(a, b)[0]
+
+
+def solve_dio(a: int, b: int, c: int) -> Tuple[int, int]:
+    #TODO fix this
+    m, x, y = ggT_mxy(a, b)
+    q = c//(a*x+b*y)
+    return q*x, q*y
+
+
+def kgV(a: int, b: int) -> int:
+    return (a*b)//ggT(a, b)
 
 
 if __name__ == '__main__':
@@ -58,5 +81,26 @@ if __name__ == '__main__':
     for i in range(5):
         a = random.randint(100, 10**5)
         b = random.randint(100, 10**5)
-
         print(ggT(a, b) == brut_ggt(a, b))
+
+    print("\nTEST solve_dio:")
+    for i in range(5):
+        a = random.randint(100, 10**3)
+        b = random.randint(100, 10**3)
+        c = random.randint(100, 10**3)
+        while c < a or c < b:
+            c = random.randint(10000, 10**6)
+        x, y = solve_dio(a, b, c)
+        print(a*x + b*y == c)
+
+    print("\nTEST kgV:")
+    def brut_kgV(a: int, b: int) -> int:
+        if a > b:
+            return brut_kgV(b, a)
+        for i in range(a, (a*b)+1):
+            if i%a == 0 and i%b == 0:
+                return i
+    for i in range(5):
+        a = random.randint(100, 10**3)
+        b = random.randint(100, 10**3)
+        print(kgV(a, b) == brut_kgV(a, b))
